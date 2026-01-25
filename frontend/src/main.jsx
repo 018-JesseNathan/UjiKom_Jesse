c82fda42ae2d6c851d90208b1eca6abd5e7db998
JesseNathan <rickumjesse@gmail.com>
2026-01-25 16:48:34 +0700

    third-commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
-import LoginPage from './Register.jsx'
+import LoginPage from './Pages/Register.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
